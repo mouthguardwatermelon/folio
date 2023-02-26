65fdcff8045f748f280f599fb24c6914d216f24a
mouthguardwatermelon <125534610+mouthguardwatermelon@users.noreply.github.com>
2023-02-26 22:59:13 +0800

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-window.onscroll = function() {
-	var background = document.querySelector('.background');
-	var scroll = window.scrollY;
-	if (scroll > 0) {
-		background.classList.add('fade');
-	} else {
-		background.classList.remove('fade');
-	}
-};
